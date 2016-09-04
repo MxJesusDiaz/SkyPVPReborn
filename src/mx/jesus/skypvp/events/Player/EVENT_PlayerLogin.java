1b42efc385be3dd55af5c2c60e45b851bde343cd
mxjesusdiaz99@gmail.com <*-_thecoffeislife_-*JESUS>
2016-09-04 13:55:55 -0500

    + Added EventPlayerJoin

--- src/mx/jesus/skypvp/events/Player/EVENT_PlayerLogin.java
+++ src/mx/jesus/skypvp/events/Player/EVENT_PlayerLogin.java
@@ -1,7 +1,10 @@
 package mx.jesus.skypvp.events.Player;
 
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerLoginEvent;
 import org.bukkit.event.player.PlayerLoginEvent.Result;
 
@@ -15,6 +18,12 @@ public class EVENT_PlayerLogin implements Listener {
 			e.setKickMessage(Strings.prefix.trim() + "\n §fYou not are in the white-list please contact with the Server Administrator." );
 		}
 	}
-	
+
+	public void OnPlayerJoin(PlayerJoinEvent e){
+	    e.setJoinMessage(null);
+        Player p = e.getPlayer();
+
+        p.sendMessage(ChatColor.DARK_GREEN.toString() + "Hola Jesus que tal la llevas");
+    }
 	
 }
